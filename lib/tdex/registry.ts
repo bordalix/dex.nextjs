a70abd091675d08df62040b502f2a8a0c8817580
bordalix <joao.bordalo@gmail.com>
2023-08-25 15:18:56 +0100

    use tor proxy on providers with onion addresses

--- lib/tdex/registry.ts
+++ lib/tdex/registry.ts
@@ -2,6 +2,8 @@ import { NetworkNames, TradeStatusMessage } from '../constants'
 import { TDEXv2Provider, isTDEXv2Provider } from '../types'
 import axios from 'axios'
 
+const DEFAULT_TOR_PROXY = 'https://proxy.tdex.network'
+
 /**
  * Get tdex registry url based on network selected on Marina
  * @param network network name
@@ -17,6 +19,11 @@ function getRegistryURL(network: NetworkNames): string {
   return Registries[network] || Registries[NetworkNames.MAINNET]
 }
 
+/**
+ * Queries provider through axios post to check for version v2
+ * @param provider TDEXv2Provider
+ * @returns boolean
+ */
 async function respondsWithCorrectVersion(
   provider: TDEXv2Provider,
 ): Promise<boolean> {
@@ -33,6 +40,25 @@ async function respondsWithCorrectVersion(
 }
 
 /**
+ * We use the HTTP1 cleartext endpoint here provided by the public tor reverse proxy
+ * https://pkg.go.dev/github.com/tdex-network/tor-proxy@v0.0.3/pkg/torproxy#NewTorProxy
+ * host:port/<just_onion_host_without_dot_onion>/
+ * @param provider TDEXv2Provider
+ * @returns provider
+ */
+function getClearTextTorProxyUrl(provider: TDEXv2Provider): TDEXv2Provider {
+  // if no onion endpoint, nothing to do here
+  if (!provider.endpoint.includes('onion')) return provider
+  // use tor proxy
+  const url = new URL(provider.endpoint)
+  const splitted = url.hostname.split('.')
+  splitted.pop()
+  const onionPubKey = splitted.join('.')
+  const endpoint = `${DEFAULT_TOR_PROXY}/${onionPubKey}`
+  return { ...provider, endpoint }
+}
+
+/**
  * Get a list of registered providers from TDEX_REGISTRY_URL
  * @param network network name
  * @returns a list of providers
@@ -42,5 +68,8 @@ export async function getProvidersFromRegistry(
 ): Promise<TDEXv2Provider[]> {
   const res = (await axios.get(getRegistryURL(network))).data
   if (!Array.isArray(res)) throw TradeStatusMessage.InvalidRegistry
-  return res.filter(isTDEXv2Provider).filter(respondsWithCorrectVersion)
+  return res
+    .filter(isTDEXv2Provider)
+    .map(getClearTextTorProxyUrl)
+    .filter(respondsWithCorrectVersion)
 }
